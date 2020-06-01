d2ee474272a19bfd0a98c48f9ce4881d89a03373
devon1910 <62015545+devon1910@users.noreply.github.com>
2020-06-01 14:18:08 +0100

    Add files via upload
    Commit changes made

---
+++ Davidson/SchoolPortal2/SchoolPortal2/Controllers/StaffController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolPortal2.Model;
+using SchoolPortal2.Repository;
+
+namespace SchoolPortal2.Controllers
+{
+    [Route("api/staff")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        // GET api/staff/all
+
+        [HttpGet("all")]
+        public ActionResult Get()
+        {
+            var staff = StaffRepo.GetAll();
+            return Ok(staff);
+        }
+
+        // GET api/staff/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            var staff = StaffRepo.Retrieve(id);
+            return Ok(staff);
+        }
+
+        // POST api/staff/add
+        [HttpPost("add")]
+        public ActionResult AddStaff([FromBody] Staff s)
+        {
+            if (StaffRepo.Add(s))
+                return Ok("Staff has been successfully added");
+            return BadRequest("The Staff has already been added");
+        }
+
+        // PUT api/staff/update
+        [HttpPut("update")]
+        public ActionResult UpdateStaff([FromBody] Staff s)
+        {
+            if (StaffRepo.Update(s))
+                return Ok("Staff has been successfull updated");
+            return NotFound("The Staff record is not found");
+        }
+
+        // DELETE api/staff/delete
+        [HttpDelete("delete")]
+        public ActionResult Delete([FromBody] Staff s)
+        {
+            if (StaffRepo.Del(s))
+                return Ok("Staff has been successfull deleted");
+            return NotFound("The Staff record is not found");
+        }
+    }
+}
---
+++ Davidson/SchoolPortal2/SchoolPortal2/Model/Staff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPortal2.Model
+{
+    public class Staff
+    {
+        public int id { get; set; }
+        public String dept { get; set; }
+        public String sex { get; set; }
+        public String name { get; set; }
+    }
+}
